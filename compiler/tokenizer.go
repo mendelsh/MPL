3783aac6143e0ba5121cab6e1fc8a9c4bbb4168a
mendelsh <mendelshperber@gmail.com>
2025-12-14 18:26:41 +0200

    token

--- compiler/tokenizer.go
+++ compiler/tokenizer.go
@@ -1,5 +1,10 @@
 package compiler
 
+import (
+	"fmt"
+	"strconv"
+)
+
 ///		tokenizer.go
 /// 	This file contains the implementation of the tokenizer for the MPL programming language.
 ///
@@ -69,13 +74,13 @@ const (
 	TOKEN_GREATER_EQUAL // >=
 	TOKEN_AND           // and
 	TOKEN_OR            // or
-	// TOKEN_NOT         // not
-	TOKEN_BITWISE_AND // &
-	TOKEN_BITWISE_OR  // |
-	TOKEN_BITWISE_XOR // ^
-	// TOKEN_BITWISE_NOT    // ~
-	TOKEN_LEFT_SHIFT  // <<
-	TOKEN_RIGHT_SHIFT // >>
+	TOKEN_NOT           // not
+	TOKEN_BITWISE_AND   // &
+	TOKEN_BITWISE_OR    // |
+	TOKEN_BITWISE_XOR   // ^
+	TOKEN_BITWISE_NOT   // ~
+	TOKEN_LEFT_SHIFT    // <<
+	TOKEN_RIGHT_SHIFT   // >>
 	TOKEN_BINARY_OPERATO_END
 
 	// Delimiters
@@ -93,6 +98,7 @@ const (
 	TOKEN_BI_F_END
 
 	// Others
+	TOKEN_DOT
 	TOKEN_NEWLINE
 	TOKEN_INDENT
 	TOKEN_DEDENT
@@ -102,38 +108,296 @@ const (
 
 type Token struct {
 	Type  TokenType
-	Value string
+	Value any
 }
 
 type Tokenizer struct {
-	input        string
-	position     int
-	readPosition int
-	ch           byte
-	line         int
-	column       int
+	input    string
+	position int
+	curr_ch  byte
+	column   int
 }
 
 func NewTokenizer(input string) *Tokenizer {
-	t := &Tokenizer{input: input, line: 1, column: 0}
-	t.readChar()
-	return t
+	return &Tokenizer{
+		input:    input,
+		position: -1,
+		curr_ch:  0,
+		column:   -1,
+	}
 }
 
-func (t *Tokenizer) readChar() {
-	if t.readPosition >= len(t.input) {
-		t.ch = 0
-	} else {
-		t.ch = t.input[t.readPosition]
+func (t *Tokenizer) readChar() bool {
+	if t.position+1 >= len(t.input) {
+		return false
 	}
-	t.position = t.readPosition
-	t.readPosition++
-	if t.ch == '\n' {
-		t.line++
+
+	t.position++
+	t.curr_ch = t.input[t.position]
+	t.column++
+	if t.curr_ch == '\n' {
 		t.column = 0
-	} else {
-		t.column++
+	}
+
+	return true
+}
+
+func (t *Tokenizer) NextToken() Token {
+	t.consumeEmptyLines()
+	if !t.readChar() {
+		return Token{Type: TOKEN_EOF}
+	}
+
+	switch t.curr_ch {
+
+	case ' ':
+		if t.column != 0 {
+			return t.NextToken()
+		}
+
+		var i int
+		for i = 0; t.readChar() && t.curr_ch == ' '; i++ {
+		}
+		return Token{Type: TOKEN_INDENT, Value: i}
+
+	case '\n':
+		return Token{Type: TOKEN_NEWLINE}
+
+	case '=':
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '=' {
+			t.readChar()
+			return Token{Type: TOKEN_EQUAL}
+		}
+		return Token{Type: TOKEN_ASSIGN}
+
+	case '+':
+		return Token{Type: TOKEN_PLUS}
+
+	case '-':
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '>' {
+			t.readChar()
+			return Token{Type: TOKEN_ARROW}
+		}
+		return Token{Type: TOKEN_MINUS}
+
+	case '*':
+		return Token{Type: TOKEN_ASTERISK}
+
+	case '/':
+		return Token{Type: TOKEN_SLASH}
+
+	case '%':
+		return Token{Type: TOKEN_PERCENT}
+
+	case '<':
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '=' {
+			t.readChar()
+			return Token{Type: TOKEN_LESS_EQUAL}
+		}
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '<' {
+			t.readChar()
+			return Token{Type: TOKEN_LEFT_SHIFT}
+		}
+		return Token{Type: TOKEN_LESS_THAN}
+
+	case '>':
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '=' {
+			t.readChar()
+			return Token{Type: TOKEN_GREATER_EQUAL}
+		}
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '>' {
+			t.readChar()
+			return Token{Type: TOKEN_RIGHT_SHIFT}
+		}
+		return Token{Type: TOKEN_GREATER_THAN}
+
+	case '!':
+		if t.position+1 < len(t.input) && t.input[t.position+1] == '=' {
+			t.readChar()
+			return Token{Type: TOKEN_NOT_EQUAL}
+		}
+		return Token{Type: TOKEN_NOT}
+
+	case '&':
+		return Token{Type: TOKEN_BITWISE_AND}
+
+	case '|':
+		return Token{Type: TOKEN_BITWISE_OR}
+
+	case '^':
+		return Token{Type: TOKEN_BITWISE_XOR}
+
+	case '~':
+		return Token{Type: TOKEN_BITWISE_NOT}
+
+	case '(':
+		return Token{Type: TOKEN_LPAREN}
+
+	case ')':
+		return Token{Type: TOKEN_RPAREN}
+
+	case ',':
+		return Token{Type: TOKEN_COMMA}
+
+	case '.':
+		return Token{Type: TOKEN_DOT}
+
+	case '"':
+		p := t.position + 1
+		for t.readChar() && t.curr_ch != '"' {
+		}
+		return Token{Type: TOKEN_STRING, Value: t.input[p:t.position]}
+	}
+
+	if !isLetter(t.curr_ch) && !isDigit(t.curr_ch) && t.curr_ch != '_' {
+		return Token{Type: TOKEN_UNKNOWN, Value: string(t.curr_ch)}
+	}
+
+	w, l := getFirstWord(t.input[t.position:])
+	fmt.Printf("DEBUG: w='%s', len=%d, curr_ch='%c'\n", w, l, t.curr_ch)
+	t.position += l - 1
+	t.curr_ch = t.input[t.position]
+	t.column += l - 1
+
+	if num, ok := parseNumber(w); ok {
+		return Token{Type: TOKEN_NUMBER, Value: num}
+	}
+
+	switch w {
+
+	case "let":
+		return Token{Type: TOKEN_LET}
+
+	case "if":
+		return Token{Type: TOKEN_IF}
+
+	case "else":
+		return Token{Type: TOKEN_ELSE}
+
+	case "repeat":
+		return Token{Type: TOKEN_REPEAT}
+
+	case "func":
+		return Token{Type: TOKEN_FUNC}
+
+	case "return":
+		return Token{Type: TOKEN_RETURN}
+
+	case "true", "false":
+		return Token{Type: TOKEN_BOOLEAN, Value: w == "true"}
+
+	case "and":
+		return Token{Type: TOKEN_AND}
+
+	case "or":
+		return Token{Type: TOKEN_OR}
+
+	case "not":
+		return Token{Type: TOKEN_NOT}
+
+	case "print":
+		return Token{Type: TOKEN_PRINT}
+
+	case "sleep":
+		return Token{Type: TOKEN_SLEEP}
+
+	default:
+		return Token{Type: TOKEN_IDENTIFIER, Value: w}
 	}
 }
 
-// func (t )
+// func (t *Tokenizer) consumeEmptyLines() {
+// 	position := t.position
+// 	curr_ch := t.curr_ch
+// 	column := t.column
+
+// 	if !t.readChar() {
+// 		return
+// 	}
+
+// 	if t.column != 0 || (t.curr_ch != ' ' && t.curr_ch != '\n') {
+// 		return
+// 	}
+
+// 	for t.readChar() && t.curr_ch == ' ' {
+// 	}
+// 	if t.curr_ch != '\n' {
+// 		t.position = position
+// 		t.curr_ch = curr_ch
+// 		t.column = column
+// 	}
+
+// 	t.consumeEmptyLines()
+// }
+
+func (t *Tokenizer) consumeEmptyLines() {
+	// if we haven't read a char yet, do nothing
+	// NextToken will call readChar() after this
+	if t.curr_ch == 0 {
+		return
+	}
+
+	// if we just returned a newline token, don't consume anything
+	// NextToken will read the next character
+	if t.curr_ch == '\n' {
+		return
+	}
+
+	for {
+		if t.column != 0 {
+			return
+		}
+
+		savedPos := t.position
+		savedCol := t.column
+		savedCh := t.curr_ch
+
+		for t.curr_ch == ' ' {
+			if !t.readChar() {
+				return
+			}
+		}
+
+		if t.curr_ch == '\n' {
+			if !t.readChar() {
+				return
+			}
+			continue
+		}
+
+		t.position = savedPos
+		t.column = savedCol
+		t.curr_ch = savedCh
+		return
+	}
+}
+
+func getFirstWord(s string) (string, int) {
+	for i, r := range s {
+		if !(r == '_' ||
+			(r >= 'a' && r <= 'z') ||
+			(r >= 'A' && r <= 'Z') ||
+			(r >= '0' && r <= '9')) {
+			return s[:i], i
+		}
+	}
+	return s, len(s)
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func parseNumber(s string) (float64, bool) {
+	if i, err := strconv.ParseInt(s, 0, 64); err == nil {
+		return float64(i), true
+	}
+	if f, err := strconv.ParseFloat(s, 64); err == nil {
+		return f, true
+	}
+	return 0, false
+}
